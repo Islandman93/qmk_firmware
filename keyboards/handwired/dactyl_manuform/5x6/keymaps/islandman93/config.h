8af9eccf1b1196a004af1a5e93269315c42f42c7
IslandMan93 <islandman93@gmail.com>
2018-09-04 13:00:24 -0400

    better layout

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/islandman93/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/islandman93/config.h
@@ -25,5 +25,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_RIGHT
 //#define EE_HANDS
 // Rows are doubled-up
-#define PERMISSIVE_HOLD
+// #define PERMISSIVE_HOLD
 #define TAPPING_TERM 75
